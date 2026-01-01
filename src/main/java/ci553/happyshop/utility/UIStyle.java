d1de68332b24be1a0f1d9d9206ceab3c9cc54000
Max Forgeron <maxforgeron@gmail.com>
2026-01-01 17:12:55 +0000

    Changed multiple colours within the GUI to make it look more enticing when a customer uses it

--- src/main/java/ci553/happyshop/utility/UIStyle.java
+++ src/main/java/ci553/happyshop/utility/UIStyle.java
@@ -56,15 +56,15 @@ public final class UIStyle {
     public static final int EmergencyExitWinHeight = 300;
 
     public static final String labelTitleStyle = "-fx-font-weight: bold; " +
-            "-fx-font-size: 16px; -fx-text-fill: purple;";
+            "-fx-font-size: 16px; -fx-text-fill: #1E3A8A;";
 
     public static final String labelStyle = "-fx-font-weight: bold; " +
             "-fx-font-size: 14px; " +
-            "-fx-text-fill: black; " +
-            "-fx-background-color: lightblue;";
+            "-fx-text-fill: Black; " +
+            "-fx-background-color: #ADD8E6;";
 
     public static final String labelLowStockStyle =
-            "-fx-font-size: 12px; -fx-text-fill: red;";
+            "-fx-font-size: 12px; -fx-text-fill:#ADD8E6;";
 
     public static final String comboBoxStyle = "-fx-font-weight: bold; " +
             "-fx-font-size: 14px;";
@@ -72,31 +72,37 @@ public final class UIStyle {
     public static final String buttonStyle = "-fx-font-size: 15";
 
     public static final String rootStyle = "-fx-padding: 8px; " +
-            "-fx-background-color: lightgreen";
+            "-fx-background-color: #FFFFFF";
 
     public static final String rootStyleBlue = "-fx-padding: 8px; " +
-            "-fx-background-color: lightblue";
+            "-fx-background-color: #ADD8E6";
 
     public static final String rootStyleGray = "-fx-padding: 8px; " +
-            "-fx-background-color: lightgray";
+            "-fx-background-color: #ADD8E6";
 
     public static final String rootStyleWarehouse = "-fx-padding: 8px; " +
-            "-fx-background-color: lightpink";
+            "-fx-background-color: #FFFFFF";
 
     public static final String rootStyleYellow = "-fx-padding: 8px; " +
-            "-fx-background-color: lightyellow";
+            "-fx-background-color: #ADD8E6";
 
     public static final String rootVipCustomerStyle = "-fx-padding: 8px; " +
-            "-fx-background-color: burlywood";
+            "-fx-background-color: #FFFFFF";
 
     public static final String spinnerArrowStyle = "-fx-font-size: 12px; -fx-padding: 0;";
 
-    public static final String textFiledStyle = "-fx-font-size: 16";
-    public static final String smallTextFiledStyle = "-fx-font-size: 14";
-    public static final String tinyTextFiledStyle = "-fx-font-size: 12";
+    public static final String textFiledStyle =
+            "-fx-font-size: 16px;" +
+                    "-fx-background-color: #ADD8E6;" +
+                    "-fx-text-fill: #111827;" +
+                    "-fx-prompt-text-fill: #ADD8E6;" +
+                    "-fx-background-radius: 6px;" +
+                    "-fx-border-radius: 6px;" +
+                    "-fx-border-color: #ADD8E6;";
+
 
     public static final String labelMulLineStyle = "-fx-font-size: 16px; " +
-            "-fx-background-color: lightpink";
+            "-fx-background-color: #ADD8E6";
 
     public static final String labelPriceStyle = "-fx-font-size: 16px; " +
             "-fx-background-color: lightyellow";
